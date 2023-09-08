7106f3f5a5a4d0c244ce485291955f5de741354a
RicardoColladoRothschild <ricardocolladorothschild@gmail.com>
2023-09-08 01:51:48 -0400

    create Main Medical Appointment view, nothing added yet here

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
-
+        MedicalAppointment ma = new MedicalAppointment();
+        ma.setVisible(true);
+        ma.setBounds(0,0,300,300);
+        ma.setResizable(false);
+        ma.setLocationRelativeTo(null);
     }
 }
--- src/MedicalAppointment.java
+++ src/MedicalAppointment.java
@@ -6,14 +6,22 @@ public class MedicalAppointment extends JFrame{
     private JButton btn_DoctorView;
     private JButton btn_PatientView;
     private JButton btn_ConsultationView;
+    private JPanel panel1;
 
-        public MedicalAppointment(){
+    public MedicalAppointment(){
             setLayout(null);
             setDefaultCloseOperation(EXIT_ON_CLOSE);
             setTitle("Medical Appointment");
 
+            //crating panel
+            panel1 = new JPanel();
+            panel1.setLayout(null);
+            panel1.setBounds(0,0,450,450);
+            add(panel1);
+
+
                 labelTitle = new JLabel("Medical Appontments");
                 labelTitle.setBounds(120,10,100,31);
-                add(labelTitle);
+                panel1.add(labelTitle);
         }
 }
